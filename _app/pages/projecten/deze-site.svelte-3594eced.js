09dfa8010d22e65f43db38ba244694913862b887
Tristan Lukens <311161@ubboemmius.net>
2021-11-11 19:02:20 +0100

    Updates

---
+++ _app/pages/projecten/deze-site.svelte-3594eced.js
@@ -0,0 +1,25 @@
+import{S as o,i as r,s,j as m,m as l,o as p,F as g,x as c,u as d,v as w}from"../../chunks/vendor-e6d7407a.js";import{P as k}from"../../chunks/project-ae1888e0.js";function h(a){let e,n;return e=new k({props:{name:"Deze site | tristanlukens",content:`
+
+# Deze site
+
+**11 november**
+
+Dit is mijn informaticaprofiel. Ik heb deze site gemaakt met de volgende technologie\xEBn:
+- [SvelteKit](https://kit.svelte.dev)
+- [TailwindCSS](https://tailwindcss.com) [(met de @tailwindcss/typography plugin)](https://github.com/tailwindlabs/tailwindcss-typography)
+- [SvelteMarkdown](https://www.npmjs.com/package/svelte-markdown)
+- [TypeScript](https://www.typescriptlang.org)
+
+---
+
+Ik ben er best blij mee! Ik vind het sowieso leuk om met websites te werken. Daarnaast doe ik ook wel wat met [go](https://golang.org). Ik wil [c](https://en.wikipedia.org/wiki/C_(programming_language)) leren, omdat het wat primitiver is en ik meer zal stilstaan bij wat ik schrijf.
+
+Ik heb een ontwerp gemaakt, echter heb ik me er niet perfect aan gehouden. Ik ben namelijk van mening dat, als je een ontwerp hebt gemaakt voor jezelf, je gaandeweg het ontwerp moet verbeteren. Als je voor een klant werkt is dat natuurlijk anders.
+
+![het originele ontwerp](/img/deze-site-img1.png)
+*het originele ontwerp*
+
+---
+
+*Door Tristan, donderdag 11 november (de koeien hebben staarten \u{1F42E})*
+`}}),{c(){m(e.$$.fragment)},l(t){l(e.$$.fragment,t)},m(t,i){p(e,t,i),n=!0},p:g,i(t){n||(c(e.$$.fragment,t),n=!0)},o(t){d(e.$$.fragment,t),n=!1},d(t){w(e,t)}}}class f extends o{constructor(e){super();r(this,e,null,h,s,{})}}export{f as default};
