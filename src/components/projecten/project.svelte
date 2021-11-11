ef83e2d795e1de3a36c62ee27366eb068ab615b5
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2021-11-11 17:24:20 +0100

    first project

---
+++ src/components/projecten/project.svelte
@@ -0,0 +1,14 @@
+<script>
+	import SvelteMarkdown from "svelte-markdown";
+
+	export let content;
+	export let name;
+</script>
+
+<svelte:head>
+	<title>{name}</title>
+</svelte:head>
+
+<section class="prose prose-blue mt-8 p-10 mx-auto">
+	<SvelteMarkdown source={content} />
+</section>
--- src/routes/projecten/deze-site.svelte
+++ src/routes/projecten/deze-site.svelte
@@ -1,5 +1,32 @@
 <script>
-	import SvelteMarkdown from "svelte-markdown";
+	import Project from "../../components/projecten/project.svelte";
 </script>
 
-<section class="prose" />
+<Project
+	name="Deze site | tristanlukens"
+	content={`
+
+# Deze site
+
+**11 november**
+
+Dit is mijn informaticaprofiel. Ik heb deze site gemaakt met de volgende technologieën:
+- [SvelteKit](https://kit.svelte.dev)
+- [TailwindCSS](https://tailwindcss.com) [(met de @tailwindcss/typography plugin)](https://github.com/tailwindlabs/tailwindcss-typography)
+- [SvelteMarkdown](https://www.npmjs.com/package/svelte-markdown)
+- [TypeScript](https://www.typescriptlang.org)
+
+---
+
+Ik ben er best blij mee! Ik vind het sowieso leuk om met websites te werken. Daarnaast doe ik ook wel wat met [go](https://golang.org). Ik wil [c](https://en.wikipedia.org/wiki/C_(programming_language)) leren, omdat het wat primitiver is en ik meer zal stilstaan bij wat ik schrijf.
+
+Ik heb een ontwerp gemaakt, echter heb ik me er niet perfect aan gehouden. Ik ben namelijk van mening dat, als je een ontwerp hebt gemaakt voor jezelf, je gaandeweg het ontwerp moet verbeteren. Als je voor een klant werkt is dat natuurlijk anders.
+
+![het originele ontwerp](/img/deze-site-img1.png)
+*het originele ontwerp*
+
+---
+
+*Door Tristan, donderdag 11 november (de koeien hebben staarten 🐮)*
+`}
+/>
