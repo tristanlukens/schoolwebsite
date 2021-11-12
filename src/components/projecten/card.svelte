230914c3d53315327ee1e9cc1d1cff853d35c5e7
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2021-11-12 07:29:48 +0100

    proportionised the projects cards on the project page

--- src/components/projecten/card.svelte
+++ src/components/projecten/card.svelte
@@ -5,9 +5,9 @@
 </script>
 
 <a
-	class="shadow-md rounded px-8 py-4 bg-mainblue text-white hover:-translate-y-1 transition"
+	class="shadow-md rounded px-8 py-4 bg-mainblue text-white hover:-translate-y-1 transition min-w-full"
 	href={link}
 >
 	<h2 class="font-bold text-lg">{name}</h2>
-	<p class="text-sm italic mt-1">{date}</p>
+	<p class="text-sm italic mt-1 leading-relaxed">{date}</p>
 </a>
--- src/routes/projecten.svelte
+++ src/routes/projecten.svelte
@@ -18,7 +18,9 @@
 	/>
 </svelte:head>
 
-<section class="text-center pt-16">
+<section
+	class="text-center pt-16 w-full px-20 mx-auto sm:px-0 sm:w-1/2 md:w-1/4"
+>
 	<h1 class="text-4xl font-bold">Projecten</h1>
 	<!-- cards for projects -->
 	<div class="mt-8 flex flex-col gap-4 items-center text-left pb-16">
