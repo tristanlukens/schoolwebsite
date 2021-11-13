7ed97195780932765c4ba3360b2773e24f641c93
tristanlukens <71601628+tristanlukens@users.noreply.github.com>
2021-11-13 13:24:01 +0100

    turned off Hi!

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,7 +11,8 @@
 	class="text-center flex flex-col justify-evenly items-center px-10 h-calc sm:flex-row"
 >
 	<div class="sm:flex-1">
-		<p class="text-gray-500 text-lg">Hi!</p>
+		<!-- turned off for the time being -->
+		<!-- <p class="text-gray-500 text-lg">Hi!</p> -->
 		<h1 class="font-extralight text-4xl mt-4">
 			Ik ben <span
 				class="font-bold relative after:absolute after:top-2/3 after:bottom-0 after:-right-2 after:-left-2 after:bg-gray-200 after:z-[-10]"
